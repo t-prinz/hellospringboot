cf6cd35bc5c9813a84afeba1e1cb2f31faddd5b5
Trey Prinz <t.prinz.a@gmail.com>
2023-01-03 13:53:06 -0500

    testing

--- src/main/java/io/tprinz/hellospringboot/HelloController.java
+++ src/main/java/io/tprinz/hellospringboot/HelloController.java
@@ -12,6 +12,8 @@ public class HelloController {
         public String index() {
                 System.out.println("Queried the REST endpoint");
 
+                String sqlSelectAllPersons = "SELECT * FROM tasks";
+
                 String mysql_user = System.getenv("MYSQL_USER");
                 System.out.println("MYSQL_USER : " + mysql_user);
 
@@ -26,6 +28,27 @@ public class HelloController {
 
                 String mysql_service_port = System.getenv("MYSQL_SERVICE_PORT");
                 System.out.println("MYSQL_SERVICE_PORT : " + mysql_service_port);
+
+//                String connectionUrl = "jdbc:mysql://localhost:3306/todo?serverTimezone=UTC";
+                String connectionUrl = "jdbc:mysql://" + mysql_service_host + ":3306/" + mysql_database + "?serverTimezone=UTC";
+                System.out.println("Connection string : " + connectionUrl);
+
+                try (Connection conn = DriverManager.getConnection(connectionUrl, mysql_user, mysql_password); 
+                        PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons); 
+                        ResultSet rs = ps.executeQuery()) {
+
+                        while (rs.next()) {
+                            long id = rs.getLong("id");
+        //                    String name = rs.getString("FIRST_NAME");
+        //                    String lastName = rs.getString("LAST_NAME");
+                              System.out.println( "id: "+id );
+
+                            // do something with the extracted data...
+                        }
+                } catch (SQLException e) {
+                              System.out.println( e );
+                    // handle the exception
+                }
 
                 return "Greetings from Spring Boot!";
         }
